a821569868c77537bf9ce525f70d4c07ef19be4e
liron_farzam <lironfa@edu.hac.ac.il>
2022-03-16 15:27:58 +0200

    w5e3 by Liron Farzam

--- w5e3.py
+++ w5e3.py
@@ -1,5 +1,4 @@
 "w5e3"
-
 #Parseltongue
 """
 The course logo appears in the resources / logo.jpg file, and inside it are hidden some secret messages.
@@ -9,10 +8,24 @@ Keep in mind that the file may be very large, and it is best not to read it all 
 Find help on the Internet regarding opening files in a binary form and a graded reading of the file.
 Be careful not to use techniques we have not learned (or add them only in addition to such a solution).
 """
-
+# open file binary mood
 f = open('logo.jpg', 'rb')
-file_content = f.read()
+content = f.read()
+str =""
+for i in range(len(content)):
+    if content[i] == ord("!"):
+        j = i - 1
+        # loop for read all the latter's (a-z only)
+        while(j>0 and content[j] <= ord("z") and content[j] >= ord("a")):
+            str += chr(content[j])
+            j = j-1
+        # just words longer then 2
+        if len(str) > 2:
+            #Reverse printing because we read the letters from end to beginning
+            print(str[::-1])
+        # reset str
+        str =""
 
-for i in file_content:
-    print(i)
 f.close()
+
+
